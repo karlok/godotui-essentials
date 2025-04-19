1fdb41a716840915b51ad5a0e553e9025fe9ee30
karlo kilayko <kkilayko@wolfko.com>
2025-04-19 09:38:51 -0400

    fixed a small plugin config error

--- plugin.gd
+++ plugin.gd
@@ -1,8 +1,11 @@
+@tool
 extends EditorPlugin
 
 func _enter_tree():
+	print("✅ GUI Essentials plugin loaded.")
 	# Optional: Add GUI components to creation menu
-	add_custom_type("GUIPanel", "Panel", preload("res://addons/godotui_essentials/gui_panel/gui_panel.gd"), preload("res://addons/godotui_essentials/icon.svg"))
-
+	add_custom_type("GUIPanel", "Panel", preload("res://addons/gui_essentials/gui_panel/gui_panel.gd"), preload("res://addons/gui_essentials/icon.svg"))
+	
 func _exit_tree():
+	print("🛑 GUI Essentials plugin unloaded.")
 	remove_custom_type("GUIPanel")
